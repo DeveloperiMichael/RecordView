80734e14301f12c191396648a643b30df586479f
张炯 <michael.zhang@wwwarehouse.com>
2017-06-01 16:42:12 +0800

    Initial Commit

---
+++ RecordDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecordDemo
+//
+//  Created by 张炯 on 17/6/1.
+//  Copyright © 2017年 张炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
